46cc50b7040a4c91b0ef04d158bc7bf90ce6f3bd
KrishnaMoorthy <krishnamoorthy@KrishnaMoorthys-MacBook-Pro.local>
2019-02-22 13:37:01 +0530

    Tuple packing and unpacking

--- Python_tuple.py
+++ Python_tuple.py
@@ -3,3 +3,11 @@ tup1 = ('Robert', 'Carlos','1965','Terminator 1995', 'Actor','Florida');
 tup2 = (1,2,3,4,5,6,7);
 print(tup1[0])
 print(tup2[1:4])
+
+#tuple packing
+x = ("Guru99", 20, "Education")
+print(x[2])
+
+#tuple unpacking
+(company, emp, profile) = x
+print(emp)
